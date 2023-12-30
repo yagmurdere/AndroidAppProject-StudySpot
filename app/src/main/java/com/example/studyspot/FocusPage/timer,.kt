3ffdf4aeab75e8e5173fbca061ac2c0eb9b7795e
yagmurdere <127977948+yagmurdere@users.noreply.github.com>
2023-12-30 15:55:03 +0300

    commit yağmur2

--- app/src/main/java/com/example/studyspot/FocusPage/timer,.kt
+++
@@ -1,9 +0,0 @@
-package com.example.studyspot.FocusPage
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun timer(){
-    Text(text = "yamue")
-}
---
+++ app/src/main/java/com/example/studyspot/Focus_Screen/timer.kt
@@ -0,0 +1,8 @@
+package com.example.studyspot.Focus_Screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun timerPomodoro(){
+
+}
