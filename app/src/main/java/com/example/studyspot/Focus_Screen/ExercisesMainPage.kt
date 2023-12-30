3e782c27e26dba57eb366b1dfe1dc11243d00c8d
yagmurdere <127977948+yagmurdere@users.noreply.github.com>
2023-12-30 20:27:35 +0300

    yağmur commit 5

--- app/src/main/java/com/example/studyspot/Focus_Screen/ExercisesMainPage.kt
+++ app/src/main/java/com/example/studyspot/Focus_Screen/ExercisesMainPage.kt
@@ -1,6 +1,7 @@
 package com.example.studyspot.Focus_Screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,20 +11,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,9 +52,12 @@ fun mainScreen(exercisesData: exerciseViewModel,
 
 
     }
-    Text(text = "Let's Focus", fontSize = 45.sp, color = colorResource(id = R.color.focusTextColor))
-    Spacer(modifier = Modifier.padding(bottom = 20.dp))
-    LazyColumn(contentPadding = PaddingValues(16.dp)){
+    Row(modifier = Modifier.padding(start = 44.dp, top = 76.dp)) {
+        Text(text = "Let's Focus", fontSize = 45.sp, fontWeight = FontWeight.Bold,color = colorResource(id = R.color.focusTextColor))
+
+    }
+
+    LazyColumn(contentPadding = PaddingValues(top = 155.dp, start = 20.dp, end = 20.dp)){
 
         val itemCount=exercisesData.exerciseNames.size
         items(itemCount){ item->
@@ -66,18 +78,16 @@ fun ColumnItem(
     title: Array<String>,
     navController: NavController
 ) {
-    Card (modifier = Modifier
+    Box (modifier = Modifier
         .padding(10.dp)
-        .wrapContentSize()
-        .clickable { navController.navigate("DetailScreen/$itemIndex") },
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White.copy(alpha = 0.5f)
-        ),
-        elevation = CardDefaults.cardElevation(1.dp))
+        .fillMaxSize()
+        .height(66.dp)
+        .clip(shape = RoundedCornerShape(15.dp))
+        .background(color = colorResource(id = R.color.focusCardBG).copy(alpha = 0.5f))
+        .clickable { navController.navigate("DetailScreen/$itemIndex") })
     {
-        Row(modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.spacedBy(15.dp))
+        Row(modifier = Modifier.fillMaxSize().padding(start = 10.dp),
+            verticalAlignment = Alignment.CenterVertically)
         {
             Text(text = title[itemIndex],
                 fontWeight = FontWeight.Bold,
