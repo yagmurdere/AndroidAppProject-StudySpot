fe3679ae05275e23ace4b399cf390d28c07258c9
dogalkn <doga.alkan@std.yeditepe.edu.tr>
2023-12-30 15:57:05 +0300

    commit dogadeneme

--- app/src/main/java/com/example/studyspot/ProfileScreen/ProfilSayfasi.kt
+++ app/src/main/java/com/example/studyspot/ProfileScreen/ProfilSayfasi.kt
@@ -1,8 +1,15 @@
 package com.example.studyspot.ProfileScreen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun ProfilSayfasi(){
+
+    Column(){
+        Text(text = "Deneme")
+
+    }
 
 }
