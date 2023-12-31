6207f3108b43f24b1266101b2a04082aa9fe7447
yagmurdere <127977948+yagmurdere@users.noreply.github.com>
2023-12-31 20:08:05 +0300

    adding map image

--- app/src/main/java/com/example/studyspot/Map_Screen/mainMapScreen.kt
+++ app/src/main/java/com/example/studyspot/Map_Screen/mainMapScreen.kt
@@ -1,2 +1,26 @@
 package com.example.studyspot.Map_Screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.studyspot.R
+import com.example.studyspot.ui.theme.StudySpotTheme
+
+
+@Composable
+fun mapMain(){
+    Column (modifier=Modifier.fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.map), contentDescription =" " )
+    }
+}
+@Composable
+@Preview
+fun mapPreview(){
+    StudySpotTheme {
+        mapMain()
+    }
+}
