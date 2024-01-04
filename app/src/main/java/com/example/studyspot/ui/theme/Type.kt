8e0e5a2c795fc8a4137579a7289b76dd68bc2f9e
ezgie <eezgiefe34@gmail.com>
2024-01-04 23:18:46 +0300

    Login sayfasını ekledim. drawable dosyasına ilgili imageları ekledim. Resources dosyasına Font dosyasıyla beraber fontu ekledim ve Type dosyasına da yeni fontu ekledm. Color dosyasına 2 yeni renk ekledim.

--- app/src/main/java/com/example/studyspot/ui/theme/Type.kt
+++ app/src/main/java/com/example/studyspot/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.studyspot.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.studyspot.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,8 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val newfontfamily= FontFamily(
+    Font(R.font.afacad)
 )
