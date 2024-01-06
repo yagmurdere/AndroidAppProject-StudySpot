169d5242d2ecf8f809571251d52301934393dae0
EmreTanrisever <emretanrisever.34@hotmail.com>
2024-01-06 22:50:46 +0300

    Add dummy restaurant to fill our mapdetail screen

--- app/src/main/java/com/example/studyspot/utilities/navigation/NavigationSetup.kt
+++ app/src/main/java/com/example/studyspot/utilities/navigation/NavigationSetup.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.studyspot.entities.RestaurantModel
 import com.example.studyspot.modules.login.Login
 import com.example.studyspot.modules.mapdetail.MapDetail
 import com.example.studyspot.modules.signup.SignUp
@@ -24,7 +25,15 @@ fun NavigationSetup(
         composable(
             route = Screen.MapDetail.route
         ) {
-            MapDetail(navController = navController)
+            val dummyRestaurant = RestaurantModel(
+                "Top Reastary",
+                "10:00-24:00",
+                "Yeni Çamlıca, M, Reşit Paşa Cd. No: 6, 34779 Ataşehir/İstanbul",
+                true,
+                true,
+                true
+            )
+            MapDetail(navController = navController, restaurant = dummyRestaurant)
         }
         composable(
             route = Screen.Login.route
